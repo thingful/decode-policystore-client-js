dd2aa8d6f5fd51054098c271643fe6e52e523b59
Sam Mulube <sam@umbrellium.co.uk>
2018-10-12 18:23:38 +0100

    Add plugin to clean the output folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
-var path = require('path');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/policystore_pb_twirp.js',
+  plugins: [
+    new CleanWebpackPlugin(['dist'])
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'decode-policystore-client.js',
+    filename: 'decode-policystore.bundle.js',
     library: 'decodePolicyStore'
   }
 };
